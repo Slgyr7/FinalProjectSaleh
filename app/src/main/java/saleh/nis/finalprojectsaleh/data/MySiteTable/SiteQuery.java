0aa404b090d982e8a64f36e5ccc11ff88ee747e6
class01 <nissaleh65@gmail.com>
2025-11-13 09:35:47 +0200

    provments

---
+++ app/src/main/java/saleh/nis/finalprojectsaleh/data/MySiteTable/SiteQuery.java
@@ -0,0 +1,31 @@
+package saleh.nis.finalprojectsaleh.data.MySiteTable;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SiteQuery {
+    @Query("SELECT * FROM site order by name desc")
+    List<site> getAllSite();
+
+    @Query("SELECT * FROM site WHERE name =:N")
+    List<site> getAllSiteByReview(String N);
+
+    @Query("SELECT * FROM site WHERE name =:N AND Cost =:Cost ")
+    List<site> getAllSiteByReview(String N, Integer Cost);
+    @Insert
+    void insertAll(site... sites);
+    @Delete
+    void delete(site site);
+    @Query("DELETE FROM site WHERE KeyId=:kid")
+    void deletesite(int kid);
+    @Update
+    void update(site site);
+
+
+}
--- app/src/main/java/saleh/nis/finalprojectsaleh/data/MySiteTable/site.java
+++ app/src/main/java/saleh/nis/finalprojectsaleh/data/MySiteTable/site.java
@@ -1,4 +1,134 @@
 package saleh.nis.finalprojectsaleh.data.MySiteTable;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class site {
+    @PrimaryKey(autoGenerate = true)
+    public long keyid;
+@ColumnInfo
+    public int name ;
+    public int Category;
+    public int Rating ;
+    public int Opentime;
+    public int CloseTime;
+    public int Cost;
+    public int Image;
+    public int Review;
+    public int Address;
+    public int Phone;
+    public int Website;
+    @Override
+    public String toString() {
+        return "site{" +
+                "keyid=" + keyid +
+                ", name=" + name +
+                ", Category=" + Category +
+                ", Rating=" + Rating +
+                ", Opentime=" + Opentime +
+                ", CloseTime=" + CloseTime +
+                ", Cost=" + Cost +
+                ", Image=" + Image +
+                ", Review=" + Review +
+                ", Address=" + Address +
+                ", Phone=" + Phone +
+                ", Website=" + Website +
+                '}';
+    }
+
+    public long getKeyid() {
+        return keyid;
+    }
+    public void setKeyid(long keyid) {
+        this.keyid = keyid;
+    }
+
+    public int getName() {
+        return name;
+    }
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getCategory() {
+        return Category;
+    }
+    public void setCategory(int category) {
+        Category = category;
+    }
+
+    public int getRating() {
+        return Rating;
+    }
+
+    public void setRating(int rating) {
+        Rating = rating;
+    }
+
+    public int getOpentime() {
+        return Opentime;
+    }
+
+    public void setOpentime(int opentime) {
+        Opentime = opentime;
+    }
+
+    public int getCloseTime() {
+        return CloseTime;
+    }
+
+    public void setCloseTime(int closeTime) {
+        CloseTime = closeTime;
+    }
+
+    public int getCost() {
+        return Cost;
+    }
+
+    public void setCost(int cost) {
+        Cost = cost;
+    }
+
+    public int getImage() {
+        return Image;
+    }
+
+    public void setImage(int image) {
+        Image = image;
+    }
+
+    public int getReview() {
+        return Review;
+    }
+
+    public void setReview(int review) {
+        Review = review;
+    }
+
+    public int getAddress() {
+        return Address;
+    }
+
+    public void setAddress(int address) {
+        Address = address;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+
+    public int getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(int website) {
+        Website = website;
+    }
+
 }
