293e25ee4fed2afe5d807156bcdafc68ecf10b99
class01 <nissaleh65@gmail.com>
2025-11-20 12:22:23 +0200

    provments

---
+++ app/src/main/java/saleh/nis/finalprojectsaleh/data/AppDataBase.java
@@ -0,0 +1,33 @@
+package saleh.nis.finalprojectsaleh.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import saleh.nis.finalprojectsaleh.data.MySiteTable.SiteQuery;
+import saleh.nis.finalprojectsaleh.data.MySiteTable.site;
+import saleh.nis.finalprojectsaleh.data.UserProfileTable.MyUser;
+import saleh.nis.finalprojectsaleh.data.UserProfileTable.MyUserQuery;
+
+@Database(entities = {MyUser.class, site.class}, version = 1)
+public abstract class AppDataBase extends RoomDatabase {
+    private static AppDataBase dp;
+
+    public abstract MyUserQuery getMyUserQuery();
+
+    public abstract SiteQuery getSiteQuery();
+
+    public static AppDataBase getDB(Context context) {
+
+        if (dp == null) {
+            dp = Room.databaseBuilder(context, AppDataBase.class, "saleh.DataBase")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return dp;
+
+    }
+}
